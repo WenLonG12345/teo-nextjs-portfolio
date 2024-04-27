1cce54c556d5d1a6f2530ac7482ca443c0a5d634
Teo Wen Long <teowenlong0316@gmail.com>
2024-04-28 01:03:06 +0800

    feat: update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: 'https://ideasss.com' || 'https://example.com',
+  siteUrl: 'https://www.teowenlong.com',
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
